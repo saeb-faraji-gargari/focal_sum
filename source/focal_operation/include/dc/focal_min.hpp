4885e44cd3e68bc8e1af23fef13f1b559e27fb52
saeb-faraji-gargari <saebfaraji@gmail.com>
2023-10-04 12:19:55 +0200

    focal_min_and_profile_performance_gprof

---
+++ source/focal_operation/include/dc/focal_min.hpp
@@ -0,0 +1,14 @@
+
+#ifndef __FOCAL_MIN_HPP
+#define __FOCAL_MIN_HPP
+
+#include "dc/raster.hpp"
+
+namespace dc {
+
+    const Raster focal_min(Raster const& input, int const& window_size_x, int const& window_size_y);
+
+}  // namespace dc
+
+#endif
+
--- source/focal_operation/source/focal_sum.cpp
+++ source/focal_operation/source/focal_sum.cpp
@@ -5,8 +5,14 @@
 
 namespace dc {
 
-    double sum_neighborhood(
-        Raster const& input, std::vector<int> const& neighborhood_node_ID, float const& no_data);
+    float sum_neighborhood(
+        Raster const& input, std::vector<int> const& neighborhood_node_ID, float const& no_data_value);
+
+    float sum_(float& argument_1, float& argument_2);
+
+    bool check_no_data(float& argument, float const& no_data_value);
+
+    bool check_out_of_range(float& argument, float& max_range_value);
 
     const Raster focal_sum(Raster const& input, int const window_size_x, int const window_size_y)
     {
@@ -14,7 +20,7 @@ namespace dc {
         int const ncols = input.size_x;
         int const nrows = input.size_y;
 
-        float const no_data = input.noData_value;
+        float const no_data_value = input.noData_value;
 
         Raster output(ncols, nrows);
 
@@ -24,43 +30,73 @@ namespace dc {
             std::vector<int> neighborhood_node_ID =
                 neighborhood_node(ncols, nrows, window_size_x, window_size_y, i);
 
-            output.value[i] = sum_neighborhood(input, neighborhood_node_ID, no_data);
+            output.value[i] = sum_neighborhood(input, neighborhood_node_ID, no_data_value);
         }
 
         return output;
     }
 
-    double sum_neighborhood(
-        Raster const& input, std::vector<int> const& neighborhood_node_ID, float const& no_data)
+    float sum_neighborhood(
+        Raster const& input, std::vector<int> const& neighborhood_node_ID, float const& no_data_value)
     {
-
-        double sum = 0.0;
+        float max_range_value {std::numeric_limits<float>::max()};
+        float sum {0.0};
 
         for (std::size_t i = 0; i < neighborhood_node_ID.size(); i++)
 
         {
+            float value_neighborhood_node = input.value[neighborhood_node_ID[i]];
 
-            if ((input.value[neighborhood_node_ID[i]] == no_data) ||
-                (std::isnan(input.value[neighborhood_node_ID[i]])))
+            if ((check_no_data(value_neighborhood_node, no_data_value)) || (std::isnan(value_neighborhood_node)))
             {
-
-                return no_data;
+                return no_data_value;
             }
 
             else
             {
 
-                sum = sum + input.value[neighborhood_node_ID[i]];
+                sum = sum_(sum, value_neighborhood_node);
 
-                if (sum >= std::numeric_limits<float>::max())
+                if (check_out_of_range(sum,max_range_value))
                 {
-
-                    return no_data;
+                    return no_data_value;
                 }
             }
         }
 
         return sum;
+    }
+
+    float sum_(float& argument_1, float& argument_2)
+
+    {
+        return argument_1 + argument_2;
+    }
+
+    bool check_no_data(float& argument, float const& no_data_value)
+
+    {
+        if (argument == no_data_value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+    }
+
+    bool check_out_of_range(float& argument, float& max_range_value)
+
+    {
+        if (argument >= max_range_value)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 
 }  // namespace dc
